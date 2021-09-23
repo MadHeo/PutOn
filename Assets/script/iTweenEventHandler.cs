5e582b7229c56ea3eadc48c9845f4ffaed74a2db
MadHeo <tnlfl288@gamail.com>
2021-09-23 21:28:39 +0900

    fix: add iTween Class
    1. iTweenHandler Class
    2. iTweenUIExtensions Class

--- Assets/script/iTweenEventHandler.cs
+++ Assets/script/iTweenEventHandler.cs
@@ -36,7 +36,7 @@ public static class iTweenUIExtensions
         return eventHandler;
     }
 
-    public static void MoveTo(this RectTransform ratget, Vector2 pos,
+    public static void MoveTo(this RectTransform target, Vector2 pos,
         float time, float delay, iTween.EaseType easeType,
         System.Action onCompleteDelegate = null)
     {
@@ -44,18 +44,18 @@ public static class iTweenUIExtensions
 
         eventHandler.OnUpdateMoveDelegate = (Vector2 value) =>
         {
-            target.anchoredPostition = value;
+            target.anchoredPosition = value;
         };
 
         eventHandler.OnCompleteDelegate = onCompleteDelegate;
 
-        iTween.ValueTo(target, gameObject, iTween.Hash(
+        iTween.ValueTo(target.gameObject, iTween.Hash(
             "from", target.anchoredPosition,
             "to", pos,
             "time", time,
-            "delay", dealy,
-            "easetype", easeType
-            "onupdate", onupdate
+            "delay", delay,
+            "easetype", easeType,
+            "onupdate", "OnUpdateMove",
             "onupdatetarget", eventHandler.gameObject,
             "oncomplete", "OnComplete",
             "oncompletetarget", eventHandler.gameObject
