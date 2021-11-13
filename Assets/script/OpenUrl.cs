bf3f850f7c829e42fc1d3a0591aa71f578e5fc43
MadHeo <tnlfl288@gamail.com>
2021-11-13 16:22:20 +0900

    feat : Add Button URL

---
+++ Assets/script/OpenUrl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    public void StuUrl()
+    {
+        Application.OpenURL("https://stuoffice.com/product/curved-banding-skirt-ivory/1029/category/24/display/1/");
+    }
+
+    public void VeilgaUrl()
+    {
+        Application.OpenURL("https://veilga.com/product/list.html?cate_no=24");
+    }
+
+    public void StuUrl2()
+    {
+        Application.OpenURL("https://stuoffice.com/product/lettering-t-shirt-navy-mens/1051/category/24/display/1/");
+    }
+}
